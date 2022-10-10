f7af9ae6014da2f07061cbd28af24cb962f0295d
Ron Cohen <ronc7500@gmail.com>
2022-10-10 12:57:40 +0300

    Add Context API

--- src/components/TargetItem.jsx
+++ src/components/TargetItem.jsx
@@ -5,14 +5,14 @@ import TargetDate from "./TargetDate"
 import { FaCheck } from "react-icons/fa"
 import { Badge } from "react-bootstrap"
 
-const TargetItem = ({ item, handleDelete, handleEdit }) => {
+const TargetItem = ({ item }) => {
 
-  // const { item, handleDelete, handleEdit } = useContext(DataContext)
+  const { deleteTarget, editTarget } = useContext(DataContext)
 
   return (
     <ListGroup.Item 
     className="d-flex justify-content-between align-items-center"
-    onDoubleClick={() => handleEdit(item.id)} >
+    onDoubleClick={() => editTarget(item.id)} >
     <h4>{item.text}</h4>
 
       <div className="d-flex justify-content-between">
@@ -21,7 +21,7 @@ const TargetItem = ({ item, handleDelete, handleEdit }) => {
    </Badge>
 
    <button style={{border: 'none', backgroundColor: '#fff'}}
-   onClick={(e) => handleDelete(e, item.id)}> 
+   onClick={(e) => deleteTarget(e, item.id)}> 
    <a href="/" style={{color: '#333'}}><FaCheck /></a>
    </button>
 
